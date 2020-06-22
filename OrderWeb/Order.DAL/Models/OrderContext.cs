588a0ced57d86dd930ef9498dcfa75c2d4ddf0fb
NguyenHue2709 <1751012028hue@ou.edu.vn>
2020-06-22 16:42:51 +0700

    Thêm Alert đặt hàng

--- OrderWeb/Order.DAL/Models/OrderContext.cs
+++ OrderWeb/Order.DAL/Models/OrderContext.cs
@@ -27,7 +27,7 @@ namespace Order.DAL.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Order;Persist Security Info=True;User ID=sa;Password=123456;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=True;");
+                optionsBuilder.UseSqlServer("Data Source=.\\HUEJH;Initial Catalog=Order;Persist Security Info=True;User ID=sa;Password=123456;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=True;");
             }
         }
 
--- OrderWeb/OrderWeb/ClientApp/src/app/main-order/main-order.component.ts
+++ OrderWeb/OrderWeb/ClientApp/src/app/main-order/main-order.component.ts
@@ -57,16 +57,16 @@ export class MainOrderComponent implements OnInit {
       var $this = $(this),
         qty = $this.parent().find('.is-form'),
         min = Number($this.attr('min')),
-        max = Number($this.attr('max'))
+        max = Number($this.attr('max')), d
       if (min == 0) {
-        var d = 0
+        d = 0
       } else d = min
       $(qty).on('click', function() {
         if ($(this).hasClass('minus')) {
           if (d > min) d += -1
         } else if ($(this).hasClass('plus')) {
-          var x = Number($this.val()) + 1
-          if (x <= max) d += 1
+          //var x = Number($this.val()) + 1
+          if (d <= max) d += 1
         }
         $this.attr('value', d).val(d)
       })
